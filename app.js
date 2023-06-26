cc8e3685f7060a57ce8b060ec5b38ea3231dba21
aspakali <aspakali9091@gmail.com>
2023-06-26 22:53:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function addition(num1, num2, num3, num4) {
+    return num1+num2+num3+num4;
+}
+function subtraction(num1, num2, num3, num4){
+    return num1-num2-num3-num4;
+}
+function multiplication(num1, num2, num3, num4){
+    return num1*num2*num3*num4;
+}
+function division(num1, num2, num3, num4) {
+    return num1/num2/num3/num4;
+}
+
+console.log(addition(7,5,6,3));
+console.log(subtraction(7,5,6,3));
+console.log(multiplication(7,5,6,3));
+console.log(division(7,5,6,3));
